87acd4ef79dfb593b8f080801df52aae0d9bd131
SettarovAli <settarov.ali@gmail.com>
2022-06-19 18:29:45 +0300

    Add mongoose and dotenv, establish connection to the MongoDB

--- src/server.js
+++ src/server.js
@@ -1,12 +1,27 @@
 const http = require('http');
+const mongoose = require('mongoose');
+const path = require('path');
+
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
 const app = require('./app');
 const { loadPlanetsData } = require('./models/planets.model');
 
 const server = http.createServer(app);
 const PORT = process.env.PORT || 5000;
+const MONGO_URL = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`;
+
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection is ready');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+});
 
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
+
   await loadPlanetsData();
 
   server.listen(PORT, () => {
