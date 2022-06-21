a01e73f8e84eb06147b27bdd0baae120c167eb84
SettarovAli <settarov.ali@gmail.com>
2022-06-21 16:14:21 +0300

    Change MONGO_URL env variable

--- src/services/mongo.js
+++ src/services/mongo.js
@@ -3,8 +3,6 @@ const path = require('path');
 
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
-const MONGO_URL = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`;
-
 mongoose.connection.once('open', () => {
   console.log('MongoDB connection is ready');
 });
@@ -14,7 +12,7 @@ mongoose.connection.on('error', (err) => {
 });
 
 async function mongoConnect() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(process.env.MONGO_URL);
 }
 
 async function mongoDisconnect() {
